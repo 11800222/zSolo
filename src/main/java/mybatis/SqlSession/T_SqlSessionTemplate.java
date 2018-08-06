5ec2670ee88a8d5304d474f7e8cc4fbe4914c578
chenguanda <chenguanda@lakala.com>
2018-08-06 11:35:01 +0800

    ...

--- src/main/java/mybatis/SqlSession/T_SqlSessionTemplate.java
+++ src/main/java/mybatis/SqlSession/T_SqlSessionTemplate.java
@@ -1,9 +1,4 @@
-/*******************************************************************************
- * Project Key : 
- * Create on 2018年8月1日 上午10:13:15
- * Copyright (c) 2004 - 2014. 拉卡拉支付有限公司版权所有. 京ICP备12007612号
- * 注意：本内容仅限于拉卡拉支付有限公司内部传阅，禁止外泄以及用于其他的商业目的
- ******************************************************************************/
+
 package mybatis.SqlSession;
 
 import java.util.concurrent.ExecutorService;
@@ -16,17 +11,10 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import mybatis.mappers.TdSystemLog;
 import mybatis.mappers.TdSystemLogMapper;
-
-/**
- * <P>TODO</P>
- * @author 陈冠达   2018年8月1日 上午10:13:15  
- */
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:applicationContext.xml")
@@ -38,7 +26,7 @@ public class T_SqlSessionTemplate {
 	public SqlSessionTemplate sqlSessionTemplate;
 
 	@Test
-	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRES_NEW, timeout = 10)
+	@Transactional
 	public void temp() throws Exception {
 		TdSystemLog t = tdSystemLogMapper.selectByPrimaryKey("1212");
 		System.out.println(t.getUpdate());
@@ -49,7 +37,6 @@ public class T_SqlSessionTemplate {
 		ExecutorService e = Executors.newCachedThreadPool();
 		for (int i = 0; i < 20; ++i) {
 			e.execute(new Runnable() {
-				@Override
 				public void run() {
 					TdSystemLog t = tdSystemLogMapper.selectByPrimaryKey("1212");
 					System.out.println(t.getUpdate());
