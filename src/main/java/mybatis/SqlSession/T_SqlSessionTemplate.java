f02d4f56f23f41cfccbb599a4ccb5add4d32253b
chenguanda <chenguanda@lakala.com>
2018-08-09 09:51:10 +0800

    w

--- src/main/java/mybatis/SqlSession/T_SqlSessionTemplate.java
+++ src/main/java/mybatis/SqlSession/T_SqlSessionTemplate.java
@@ -1,6 +1,7 @@
 
 package mybatis.SqlSession;
 
+import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -23,24 +24,23 @@ import mybatis.mappers.TdSystemLogMapper;
 public class T_SqlSessionTemplate {
 
 	@Test
-	public void temp() throws Exception {
-		transactional_Method.Query();
+	public void diff_Transactional_same_SqlSession() throws Exception {
+		//	transactional_Method.nesting_Transactional_RollBack();
+		//	another_Transactional_Method.RollBack();
+		//		another_Transactional_Method.RollBack_throw_out();
 
-	}
-
-	public static void simpleQuery() throws Exception {
-		tdSystemLogMapper.selectByPrimaryKey("1212");
 	}
 
 	@Test
-	public void Create_Use_Commit_close() throws Exception {
-		simpleQuery();
-		simpleQuery();
-		simpleQuery();//一共开闭三个DefaultSqlSession
+	public void Rollback() throws Exception {
+		another_Transactional_Method.RollBack_catch();//Exception不抛出@Transactional外不会回滚
+
+		another_Transactional_Method.RollBack_throw_out();
+
 	}
 
 	@Test
-	public void Threadsafe() throws Exception {
+	public void thread_safe() throws Exception {
 		ExecutorService e = Executors.newCachedThreadPool();
 		for (int i = 0; i < 20; ++i) {
 			e.execute(new Runnable() {
@@ -62,7 +62,20 @@ public class T_SqlSessionTemplate {
 	public static SqlSessionTemplate sqlSessionTemplate;
 
 	@Autowired
-	public Transactional_Method transactional_Method;
+	public static Transactional_Method transactional_Method;
+
+	@Autowired
+	public static Another_Transactional_Method another_Transactional_Method;
+
+	@Autowired
+	public void setTransactional_Method(Transactional_Method transactional_Method) {
+		T_SqlSessionTemplate.transactional_Method = transactional_Method;
+	}
+
+	@Autowired
+	public void setAnother_Transactional_Method(Another_Transactional_Method another_Transactional_Method) {
+		T_SqlSessionTemplate.another_Transactional_Method = another_Transactional_Method;
+	}
 
 	@Autowired
 	public void setTdSystemLogMapper(TdSystemLogMapper tdSystemLogMapper) {
@@ -77,9 +90,69 @@ public class T_SqlSessionTemplate {
 }
 
 class Transactional_Method {
+
+	@Transactional
+	public void nesting_Transactional() throws Exception {
+
+		T_SqlSessionTemplate.tdSystemLogMapper.selectByPrimaryKey("1212");
+
+		System.out.println("进入另一个类的@Transactional方法。。。。。");
+		T_SqlSessionTemplate.another_Transactional_Method.Query();
+		System.out.println("退出另一个类的@Transactional方法。。。。。");
+	}
+
+	@Transactional
+	public void nesting_Transactional_RollBack() throws Exception {
+
+		T_SqlSessionTemplate.tdSystemLogMapper.selectByPrimaryKey("1212");
+
+		System.out.println("进入另一个类的@Transactional（会rollback）方法。。。。。");
+		try {
+			T_SqlSessionTemplate.another_Transactional_Method.RollBack_throw_out();//
+		} catch (Exception e) {
+		}
+		System.out.println("退出另一个类的@Transactional（会rollback）方法。。。。。");
+	}
+
+}
+
+class Another_Transactional_Method {
+
+	@Transactional
+	public void RollBack_throw_out() throws Exception {
+		System.out.println("正常更新Notes为时间：" + String.valueOf(new Date().getTime()));
+		T_SqlSessionTemplate.sqlSessionTemplate.update("mybatis.mappers.TdSystemLogMapper.updateNotNullByPrimaryKey", new TdSystemLog("1212", String.valueOf(new Date().getTime())));//insert、delete都是走的update
+
+		System.out.println("执行异常数据库操作：");
+		StringBuffer longStr = new StringBuffer("————————————————————————");
+		for (int i = 0; i < 10; ++i) {
+			longStr.append(longStr);
+		}
+		T_SqlSessionTemplate.sqlSessionTemplate.update("mybatis.mappers.TdSystemLogMapper.updateNotNullByPrimaryKey", new TdSystemLog("1212", longStr.toString()));//insert、delete都是走的update
+
+		System.out.println("结束");
+	}
+
 	@Transactional
 	public void Query() throws Exception {
 		T_SqlSessionTemplate.tdSystemLogMapper.selectByPrimaryKey("1212");
 	}
 
+	@Transactional
+	public void RollBack_catch() {
+		System.out.println("正常更新Notes为时间：" + String.valueOf(new Date().getTime()));
+		T_SqlSessionTemplate.sqlSessionTemplate.update("mybatis.mappers.TdSystemLogMapper.updateNotNullByPrimaryKey", new TdSystemLog("1212", String.valueOf(new Date().getTime())));//insert、delete都是走的update
+
+		try {
+			System.out.println("执行异常数据库操作：");
+			StringBuffer longStr = new StringBuffer("————————————————————————");
+			for (int i = 0; i < 10; ++i) {
+				longStr.append(longStr);
+			}
+			T_SqlSessionTemplate.sqlSessionTemplate.update("mybatis.mappers.TdSystemLogMapper.updateNotNullByPrimaryKey", new TdSystemLog("1212", longStr.toString()));//insert、delete都是走的update
+		} catch (Exception e) {
+		}
+
+		System.out.println("结束");
+	}
 }
