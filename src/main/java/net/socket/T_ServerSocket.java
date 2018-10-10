89003956bbd5a32df128b7c3c24b288a75c6ff07
chenguanda <chenguanda@lakala.com>
2018-10-10 16:17:24 +0800

    16:17

--- src/main/java/net/socket/T_ServerSocket.java
+++ src/main/java/net/socket/T_ServerSocket.java
@@ -1,6 +1,7 @@
 package net.socket;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -9,18 +10,39 @@ public class T_ServerSocket {
 	public static ArrayList<Socket> clients = new ArrayList<>();
 
 	public static void main(String[] args) throws IOException {
-		openServer(18899);
+		openServer(23333);
 
 	}
 
 	public static void openServer(int port) throws IOException {
 		ServerSocket ss = new ServerSocket(port);
-		System.out.println("listen on 18899");
+		System.out.println("listen on  " + port);
 		while (true) {
-			Socket client = ss.accept();
+			final Socket client = ss.accept();
 			clients.add(client);
-			System.out.println("a client has connected to server");
+			listenThisSocket(client);
 		}
+	}
+
+	public static void listenThisSocket(final Socket client) {
+		Thread t = new Thread(new Runnable() {
+			public void run() {
+				try {
+					InputStream in = client.getInputStream();
+					System.out.println("begin listen to client  " + client.getPort());
+					while (true) {
+						int b = in.read();
+						System.out.println("has read byte : " + b + " from client " + client.getPort());
+						if (b == -1)
+							break;
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			}
+		});
+		t.start();
 	}
 
 }
--- src/main/java/net/socket/T_Socket.java
+++ src/main/java/net/socket/T_Socket.java
@@ -1,23 +1,34 @@
 package net.socket;
 
 import java.net.Socket;
+import java.util.Scanner;
 import java.util.concurrent.CountDownLatch;
 
 public class T_Socket {
 	public static CountDownLatch countDownLatch = new CountDownLatch(2);
 
 	public static void main(String[] args) throws Exception {
-		connect(18899);
+		Socket client = connect(23333);
+		Scanner sc = new Scanner(System.in);
+		String option = sc.nextLine();
+		switch (option) {
+		case "1": {
+			client.close();
+			break;
+		}
+		case "2": {
+			client.shutdownOutput();
+			break;
+		}
+		}
+		countDownLatch.await();
 	}
 
 	public static Socket connect(int port) throws Exception {
 		Socket client = new Socket("127.0.0.1", port);
 
-		client.shutdownOutput();
+		System.out.println("Socket in port : " + client.getLocalPort() + " has connected to ");
 
-		client.shutdownInput();
-
-		countDownLatch.await();
 		return client;
 	}
 }
