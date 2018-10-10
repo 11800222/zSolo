03ed961d0e0d93d24203c04abdbcf85dec4fc5e2
11800222 <545423690@qq.com>
2018-10-10 09:20:04 +0800

    9:19

--- src/main/java/net/socket/T_ServerSocket.java
+++ src/main/java/net/socket/T_ServerSocket.java
@@ -1,10 +1,26 @@
 package net.socket;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class T_ServerSocket {
+	public static ArrayList<Socket> clients = new ArrayList<>();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		openServer(18899);
 
+	}
+
+	public static void openServer(int port) throws IOException {
+		ServerSocket ss = new ServerSocket(port);
+		System.out.println("listen on 18899");
+		while (true) {
+			Socket client = ss.accept();
+			clients.add(client);
+			System.out.println("a client has connected to server");
+		}
 	}
 
 }
--- src/main/java/net/socket/T_Socket.java
+++ src/main/java/net/socket/T_Socket.java
@@ -1,9 +1,23 @@
 package net.socket;
 
+import java.net.Socket;
+import java.util.concurrent.CountDownLatch;
+
 public class T_Socket {
+	public static CountDownLatch countDownLatch = new CountDownLatch(2);
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws Exception {
+		connect(18899);
 	}
 
+	public static Socket connect(int port) throws Exception {
+		Socket client = new Socket("127.0.0.1", port);
+
+		client.shutdownOutput();
+
+		client.shutdownInput();
+
+		countDownLatch.await();
+		return client;
+	}
 }
