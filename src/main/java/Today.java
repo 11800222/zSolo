55cc00f6fdecb9819095e5afe49d405d86a4f3db
uncledrew22 <545423690@qq.com>
2019-04-23 20:27:32 +0800

    ForTest

--- src/main/java/Today.java
+++ src/main/java/Today.java
@@ -3,7 +3,9 @@ import java.io.IOException;
 public class Today {
 
 	public static void main(String[] args) throws InterruptedException, IOException {
-
+		System.out.println(332);
+		System.out.println(33);
+		System.out.println(33);
 	}
 
 }
