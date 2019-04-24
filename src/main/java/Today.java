bdc427069a32547ce6687c85d7ab897ecc541f8e
uncledrew22 <545423690@qq.com>
2019-04-24 08:59:22 +0800

    forward localNewBranch

--- src/main/java/Today.java
+++ src/main/java/Today.java
@@ -5,7 +5,7 @@ public class Today {
 	public static void main(String[] args) throws InterruptedException, IOException {
 		System.out.println(332);
 		System.out.println(33);
-		System.out.println(33);
+
 	}
 
 }
